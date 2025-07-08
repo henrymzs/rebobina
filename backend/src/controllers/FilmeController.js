6fdfb61a8bbc60131928bc9d7828f7a4b530ba0a
henrymzs <henrykaua21@gmail.com>
2025-07-08 12:49:05 -0300

    fix: refaz o controller passando a logica para o service

--- backend/src/controllers/FilmeController.js
+++ backend/src/controllers/FilmeController.js
@@ -1,49 +1,25 @@
-const axios = require('axios');
 const FilmeDAO = require('../models/DAO/FilmeDAO');
-const ListaFilmesDAO = require('../models/DAO/ListaFilmesDAO');
-require('dotenv').config();
-const apiKey = process.env.API_KEY;
+const FilmeSerivce = require('../services/FilmeService');
 
-exports.adicionarFilme = async (req, res) => {
+const adicionarFilme = async (req, res) => {
     try {
         const { titulo } = req.body;
-        const usuarioId = req.id; 
-        const lista = await ListaFilmesDAO.findByUserId(usuarioId);
-
-        if (!lista || lista.usuarioId !== usuarioId) {
-            console.log("Nenhuma lista foi encontrada para o usuário ID:", usuarioId);
-            return res.status(400).json({ erro: "Usuário ainda não criou uma lista de filmes" });
+        const usuarioId = req.usuario.id;
+        if (!titulo) {
+            return res.status(400).json({ erro: "Nome do filme é obrigatório para busca." });
         }
-
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${titulo}`);
-        const filmeTMDb = response.data.results[0]; 
-
-        if (!filmeTMDb) {
-            return res.status(404).json({ erro: "Filme não encontrado na API TMDb" });
+        const resultado = await FilmeSerivce.adicionarFilme(titulo, usuarioId);
+        if (!resultado.success) {
+            return res.status(404).json({ erro: resultado.message });
         }
-        const listaId = lista?.id;
-        console.log("🔍 ID da lista extraído:", listaId);
-        console.log("🔍 Tentando adicionar filme à lista ID:", listaId);
-        console.log("🔍 Usuário autenticado:", usuarioId);
-        try {
-            const novoFilme = await FilmeDAO.create({
-            id_tmdb: filmeTMDb.id, 
-            titulo: filmeTMDb.title,
-            listaId: listaId
-            });
-            res.status(201).json(novoFilme);
-        } catch (error) {
-            res.status(404).json({ erro: error.message });
-        }
+        return res.status(201).json(resultado.filme);
     } catch (error) {
-        res.status(500).json({ erro: "Erro ao adicionar filme" });
-        console.error('erro', error);
-        throw error;
-
+        console.error('Erro ao adicionar filme:', error);
+        return res.status(500).json({ erro: "Erro interno ao adicionar filme." });
     }
 };
 
-exports.editarFilme = async (req, res) => {
+const editarFilme = async (req, res) => {
     try {
         const { id } = req.params;
         const { titulo } = req.body;
@@ -57,7 +33,7 @@ exports.editarFilme = async (req, res) => {
     }
 }
 
-exports.excluirFilme = async (req, res) => {
+const excluirFilme = async (req, res) => {
     try {
         const { id } = req.params;
         const resultado = await FilmeDAO.filmeDelete(id);
@@ -66,3 +42,5 @@ exports.excluirFilme = async (req, res) => {
         res.status(400).json({ erro: message || 'Erro ao excluir filme.' })
     }
 };
+
+module.exports = { adicionarFilme, editarFilme, excluirFilme };

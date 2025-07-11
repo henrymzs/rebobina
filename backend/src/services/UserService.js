7ae547fd6b2e87c1fdda0982cbff84c8d47b1819
henrymzs <henrykaua21@gmail.com>
2025-07-10 22:52:44 -0300

    feat: adiciona service da rota para usuario deletar sua conta

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -28,6 +28,17 @@ const deleteUser = async (usuarioId) => {
     return { success: true, message: 'Usuário excluído com sucesso.' };
 };
 
+const deleteUserAccount = async (usuarioId) => {
+    const listasDoUsuario = await ListaFilmesDAO.findByUserId(usuarioId);
+    if (Array.isArray(listasDoUsuario) && listasDoUsuario.length > 0) {
+        for (const lista of listasDoUsuario) {
+            await ListaFilmesDAO.deleteLista(lista.id);
+        }
+    }
+    await UsuarioDAO.delete(usuarioId);
+    return { success: true, message: 'Usuário excluído com sucesso.' };
+};
+
 const updateNameList = async (usuarioId, nomeLista) => {
     if (!nomeLista || nomeLista.trim() === '') {
         return { sucesso: false, mensagem: "O nome da lista é obrigatório." };
@@ -43,4 +54,4 @@ const searchUserList = async (usuarioId) => {
     return { sucesso: true, lista };
 }
 
-module.exports = { fetchAllUsers, changeUserRole, deleteUser, updateNameList, searchUserList };
+module.exports = { fetchAllUsers, changeUserRole, deleteUser, updateNameList, searchUserList, deleteUserAccount };

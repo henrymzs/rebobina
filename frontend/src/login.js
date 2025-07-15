c359824002a1620188c5caeb0fffddf6d6761fec
henrymzs <henrykaua21@gmail.com>
2025-07-14 23:42:14 -0300

    feat: adiciona arquivo js para a pagina de login para validar o email e a senha do usuario

---
+++ frontend/src/login.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const emailErro = document.getElementById('email-error');
+  const passwordErro = document.getElementById('password-error');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const validEmail = validateEmail(emailInput.value);
+    const validPassword = validatePassword(passwordInput.value);
+
+    showError(emailErro, !validEmail);
+    showError(passwordErro, !validPassword);
+
+    if (validEmail && validPassword) {
+      form.submit(); 
+    }
+  });
+});

b7d232c5da9abbd30802f583dc90c2701974b99f
henrymzs <henrykaua21@gmail.com>
2025-07-16 17:08:57 -0300

    feat: adiciona funcao para adicionar usuario ao sistema

---
+++ frontend/src/scripts/registerUser.js
@@ -0,0 +1,35 @@
+function registerUser() {
+    const form = document.getElementById('form');
+    const nome = form.name.value;
+    const email = form.email.value;
+    const senha = form.password.value;
+
+    const dados = {
+        nome: nome,
+        email: email,
+        senha: senha
+    };
+
+    fetch('http://localhost:3000/user/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Cadastro realizado com sucesso!');
+                form.reset()
+                window.location.href = 'src/templates/login.html';
+            } else {
+                const msg = data.messagem || data.message || data.erro || "Erro desconhecido";
+                alert("Erro ao cadastrar: " + msg);
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Ocorreu um erro no servidor');
+        });
+}

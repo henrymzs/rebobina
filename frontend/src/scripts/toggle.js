07b1ae337d4c14ace5f055a3b6d6ac2876cbfc80
henrymzs <henrykaua21@gmail.com>
2025-07-16 00:32:48 -0300

    fix: reajustando funcao toggle

--- frontend/src/scripts/toggle.js
+++ frontend/src/scripts/toggle.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const passwordInput = document.getElementById('password');
-  const eyePassword = document.getElementById('eye');
+  const eyePassword = document.getElementById('eye-lock');
   eyePassword.addEventListener('click', function () {
     const currentType = passwordInput.getAttribute('type');
     const type = currentType === 'password' ? 'text' : 'password';

375c47a01a5e93c40deb163b3c5ee3554e905e84
henrymzs <henrykaua21@gmail.com>
2025-07-17 22:58:00 -0300

    refactor: remove funcao antiga

--- frontend/src/scripts/toggle.js
+++ frontend/src/scripts/toggle.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const passwordInput = document.getElementById('password');
-  const eyePassword = document.getElementById('eye-lock');
-  eyePassword.addEventListener('click', function () {
-    const currentType = passwordInput.getAttribute('type');
-    const type = currentType === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-  });
-});
-
 function togglePassword() {
     const passwordInput = document.getElementById('password');
     const eyePassword = document.getElementById('eye-lock');

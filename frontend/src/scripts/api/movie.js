f51d09b6f903052c4f4dcc3f82b2f8c12d02204c
henrymzs <henrykaua21@gmail.com>
2025-07-21 22:53:46 -0300

    feat: adicionando arquivo movie e user js que faz requisicoes a api

---
+++ frontend/src/scripts/api/movie.js
@@ -0,0 +1,49 @@
+const API_URL = 'http://localhost:3000';
+
+export async function postMovie(titulo, token) {
+    return fetch(`${API_URL}/movies`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ titulo })
+    })
+        .then(res => res.json())
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Erro ao conectar com o servidor');
+        });
+}
+
+export async function deleteMovie(id, token) {
+    return fetch(`${API_URL}/movies/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+        .then(res => res.json())
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Erro ao conectar com o servidor');
+        });
+}
+
+export async function updateMovie(id, newTitle, token) {
+    return fetch(`${API_URL}/movies/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ titulo: newTitle })
+    })
+        .then(res => res.json())
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Erro ao conectar com o servidor');
+        });
+}
+

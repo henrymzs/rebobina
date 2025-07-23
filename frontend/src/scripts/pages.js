be8d00ab83e0b170cfc78dc82ac84a0add04e74c
henrymzs <henrykaua21@gmail.com>
2025-07-23 16:12:51 -0300

    feat: adiciona funcao para carregar os card do usuario na pagina main

--- frontend/src/scripts/pages.js
+++ frontend/src/scripts/pages.js
@@ -1,21 +1,22 @@
-import { initRegisterForm, initLoginForm  } from "./interface/userForm.js";
-import { togglePassword  } from "./utils/toggle.js";
+import { initRegisterForm, initLoginForm } from "./interface/userForm.js";
+import { togglePassword } from "./utils/toggle.js";
 import { initModal } from "./interface/modal.js";
-import { initAddMovie, initDeleteMovie, initEditMovie } from "./interface/movieFeatures.js"
+import { initAddMovie, initDeleteMovie, initEditMovie, loadUserMovies } from "./interface/movieFeatures.js"
 
 export function initPage() {
-  const page = document.body.dataset.page;
+    const page = document.body.dataset.page;
 
-  if (page === 'index') {
-    initRegisterForm();
-    togglePassword();
-  } else if (page === 'login') {
-    initLoginForm();
-    togglePassword();
-  } else if (page === 'main') {
-    initModal();
-    initAddMovie();
-    initDeleteMovie();
-    initEditMovie();
-  }
+    if (page === 'index') {
+        initRegisterForm();
+        togglePassword();
+    } else if (page === 'login') {
+        initLoginForm();
+        togglePassword();
+    } else if (page === 'main') {
+        initModal();
+        initAddMovie();
+        initDeleteMovie();
+        initEditMovie();
+        loadUserMovies()
+    }
 }

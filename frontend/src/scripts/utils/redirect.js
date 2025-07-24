d86131b6c7d1019d44ede34da18c2b56394ca198
henrymzs <henrykaua21@gmail.com>
2025-07-23 23:24:11 -0300

    feat: adiciona funcoes para redirecionar usuario pelas paginas alem de validar se esta logado

---
+++ frontend/src/scripts/utils/redirect.js
@@ -0,0 +1,27 @@
+export function requireAuth(redirectTo = '../templates/login.html') {
+  const token = localStorage.getItem('authToken');
+  if (!token) {
+    alert('Você precisa estar logado para acessar esta página.');
+    window.location.href = redirectTo;
+  }
+}
+
+export function logoutUser() {
+    const btnLogout = document.getElementById('logoutBtnFooter');
+
+    btnLogout.addEventListener('click', () => {
+        localStorage.removeItem('authToken');
+
+        alert('Você saiu da sua conta!');
+        window.location.href = '../templates/login.html';
+    });
+}
+
+export function settingsUser() {
+    const btnSettings = document.getElementById('accountSettingsBtn');
+    btnSettings.addEventListener('click', () => {
+        window.location.href = '../templates/settings.html';
+    });
+}
+
+

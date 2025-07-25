d2fcd4b46429fffb2334669f467b1d8f342bfde7
henrymzs <henrykaua21@gmail.com>
2025-07-24 23:35:46 -0300

    feat: adiciona funcao para abrir e fechar modal e logica para sortear aos cards

--- frontend/src/scripts/interface/modal.js
+++ frontend/src/scripts/interface/modal.js
@@ -1,21 +1,69 @@
-export function initModal() {
-  const modal = document.getElementById('addLinkModal');
-  const openModalBtn = document.getElementById('openModalBtn');
-  const closeModalBtn = document.getElementById('closeModalBtn');
+import { renderModalCard } from '../components/cardRandomMovie.js';
 
-  if (!modal || !openModalBtn || !closeModalBtn) return;
+export function openModalLink() {
+    const modal = document.getElementById('addLinkModal');
+    const openModalBtn = document.getElementById('openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
 
-  openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-  });
+    if (!modal || !openModalBtn || !closeModalBtn) return;
 
-  closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
+    openModalBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    });
 
-  window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  });
+    closeModalBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 }
+
+export function openModalCard() {
+    const modal = document.getElementById('randomModal');
+    const openBtn = document.getElementById('randomizeCardsBtn');
+    const closeBtn = document.getElementById('closeRandomModalBtn');
+
+    if (!modal || !openBtn || !closeBtn) {
+        return;
+    }
+
+    openBtn.addEventListener('click', () => {
+        const cards = document.querySelectorAll('.link-card');
+        const container = document.getElementById('randomMovieContainer');
+
+        if (!cards.length) {
+            alert('Lista vazia!');
+            return;
+        }
+
+        if (cards.length < 3) {
+            alert('Você precisa ter no minimo ter 3 links na sua lista para poder utilizar esta função.');
+            return;
+        }
+
+        const sorteado = cards[Math.floor(Math.random() * cards.length)];
+        const titulo = sorteado.querySelector('.movie-title')?.textContent;
+        if (titulo) {
+            container.innerHTML = '';
+            renderModalCard(titulo);
+            console.log(container.innerHTML);
+
+            modal.style.display = 'flex';
+        }
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
+

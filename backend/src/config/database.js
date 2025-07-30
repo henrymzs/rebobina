cc4362686359447ca578256e11ccc95d183170cd
henrymzs <henrykaua21@gmail.com>
2025-07-30 20:32:10 -0300

    feat: teste para banco de dados postgres

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,26 @@
-require('dotenv').config();
-const {Sequelize} = require('sequelize');
+//require('dotenv').config();
+//const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+//const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, //process.env.DB_PASSWORD, {
+//    host: process.env.DB_HOST,
+//    port: process.env.DB_PORT,
+//    dialect: process.env.DB_DIALECT,
+//});
+
+//module.exports = sequelize;
+
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
 });
 
 module.exports = sequelize;

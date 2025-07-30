8ab8fc0039aa64d5f788c4f7cbeecb968b7ed146
henrymzs <henrykaua21@gmail.com>
2025-07-29 23:44:37 -0300

    fix: conserta rotas no app.js

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = require('./routes/routes.js');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+class App {
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.server.use(express.json());
+        this.server.use(express.urlencoded({ extended: true }));
+        this.server.use(cookieParser());
+        this.server.use(cors({
+            origin: '*',
+            credentials: true,
+        }));
+    }
+    routes() {
+        this.server.use(router);
+    }
+}
+
+module.exports = new App().server;

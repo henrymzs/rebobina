92d42d5d3a3908d8c48375e26232051a313ecb36
henrymzs <henrykaua21@gmail.com>
2025-10-01 23:31:34 -0300

    feat: modifica app.jsx para testar componentes criados em components

--- spa/src/App.jsx
+++ spa/src/App.jsx
@@ -1,35 +1,42 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/variables.css';
+import './styles/global.css'
+import Button from './components/UI/Button'
+import Input from './components/UI/Input'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <>
+            <div>
+                <form>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                    <Input
+                        label="E-mail"
+                        type="email"
+                        name="userEmail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder="Digite seu e-mail"
+                        required
+                    />
+                    <Input
+                        label="Senha"
+                        type="password"
+                        name="userPass"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder="Digite sua senha"
+                        required
+                    />
+                </form>
+                <Button onClick={() => alert('clickou')} >Clique aqui</Button>
+            </div>
+        </>
+    )
 }
 
 export default App
